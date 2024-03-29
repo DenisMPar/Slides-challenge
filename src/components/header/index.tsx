376f0dbc5d32d7ba1aa22c57af06267f5263c6d9
Denis Parada <denispar.dev@gmail.com>
2024-03-29 15:33:35 -0300

    Feature: added language component to header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,36 @@
+"use client";
 import { HamburguerIcon } from "@/ui/icons/hamburguer-icon";
+import { Link } from "@/navigation";
+import { useState } from "react";
+import { EnglandFlag } from "@/ui/icons/england";
+import { SpainFlag } from "@/ui/icons/spain";
+import { Language } from "../language";
 
 export function Header() {
+  const [open, setOpen] = useState(false);
+  function handleClick() {
+    setOpen(!open);
+  }
   return (
-    <header className="flex justify-end px-6 pt-6 pb-3  ">
-      <HamburguerIcon className="cursor-pointer" />
+    <header className="flex justify-end px-6 xl:px-11 pb-2 color-black gap-2 lg:gap-6 ">
+      <nav
+        className={` rounded-b-lg shadow-md flex items-center px-4  ${
+          open ? "block" : "hidden"
+        } transition duration-500 ease-in-out`}
+      >
+        <ul
+          className={`flex gap-4 lg:gap-8 lg:items-center lg:justify-end lg:ml-auto`}
+        >
+          <Language />
+        </ul>
+      </nav>
+
+      <button
+        className=" focus-visible:outline-none flex items-center h-12 lg:h-16"
+        onClick={handleClick}
+      >
+        <HamburguerIcon className="cursor-pointer" />
+      </button>
     </header>
   );
 }
